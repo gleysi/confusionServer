f9e2f83cb5d35f4b455e25291630d92f677548a7
Miranda Tejada <sonia.miranda.tejada@accenture.com>
2020-07-18 19:01:27 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '2035778599888510',
+        clientSecret: '7313d2584c3ba8b44f58f4bcd0085862'
+    }
 }
